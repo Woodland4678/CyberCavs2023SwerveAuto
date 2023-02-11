329145ff38e5143b34d4d69a1799c038ca6fd5f7
Woodland4678 <cybercavs@woodland.on.ca>
2023-02-11 14:28:18 -0500

    auto balance testing

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -4,49 +4,92 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.SwerveDrive;
 
 public class AutoBalance extends CommandBase {
+  PIDController yController = new PIDController(0.035, 0.005, 0);
   SwerveDrive s_Swerve;
   int count =0;
+  int timer = 0;
   boolean isBalanced = false;
+  boolean isDone = false;
+  double[] rollArray = new double[20];
   /** Creates a new AutoBalance. */
   public AutoBalance(SwerveDrive s_Swerve) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.s_Swerve = s_Swerve;
     addRequirements(s_Swerve);
+    
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    yController.setSetpoint(-0.5);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    if (timer == 2){
+      timer =0;
+     
+      for (int i = 0; i<19; i++){
+        rollArray[i] = rollArray[i + 1];
+      }
+      rollArray[19] = s_Swerve.getGyroRoll();
+
+        SmartDashboard.putNumber(
+                    "Roll difference",Math.abs(rollArray[19] - rollArray[0]));
+    }
+    timer++;
+
+    System.out.println(rollArray[19]);
+    if (rollArray[19] != 0) {
+      
+      if (Math.abs(rollArray[19] - rollArray[0]) > 14){
+        
+        isDone = true;
+        
+      }
+    }
+    
+
+
+
+    var ySpeed = yController.calculate(s_Swerve.getGyroRoll());
+
     if (count > 50){
       count = 0;
       isBalanced = true;
     }
-
-    if (s_Swerve.getGyroRoll() > 1) {
+    //Translation2d translation = new Translation2d(-ySpeed, 0);
+    //s_Swerve.drive(translation, 0, false, true);
+    if (Math.abs(s_Swerve.getGyroRoll()) < 1) {
+      count++;
+    }
+    if (count > 25) {
+      isDone = true;
+    }
+    if (s_Swerve.getGyroRoll() > 3.5) {
       Translation2d translation = new Translation2d(0.5, 0);
       s_Swerve.drive(translation, 0, false, true);
       count = 0;
     }
-   else if (s_Swerve.getGyroRoll() < -1.5){
+   else if (s_Swerve.getGyroRoll() < -3.5){
       Translation2d translation = new Translation2d(-0.5, 0);
       s_Swerve.drive(translation, 0, false, true);
       count = 0;
     }
-    else {
-      Translation2d translation = new Translation2d(0, 0);
-      s_Swerve.drive(translation, 0, false, true);
-      count++;
-    }
+    // else {
+    //   Translation2d translation = new Translation2d(0, 0);
+    //   s_Swerve.drive(translation, 0, false, true);
+    //   count++;
+    // }
   }
 
   // Called once the command ends or is interrupted.
@@ -58,6 +101,6 @@ public class AutoBalance extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return isBalanced;
+    return isDone;
   }
 }
